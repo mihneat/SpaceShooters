67672d43f5b8f9d0331406ab46c532483c32c79e
Hyper Lucario <hiperlucario@gmail.com>
2019-05-11 22:31:31 +0000

    MainMenu

---
+++ Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject transition;
+
+    private bool animIsPlaying = false;
+
+    public void Play()
+    {
+        if (!animIsPlaying) StartCoroutine(PlayGame());
+    }
+
+    IEnumerator PlayGame()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        animIsPlaying = true;
+        transition.SetActive(true);
+
+        yield return new WaitForSeconds(3.0f);
+
+        animIsPlaying = false;
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        if (!animIsPlaying) Application.Quit();
+    }
+}
